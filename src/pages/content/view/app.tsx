1c47fe0a876aa583642432112997e0afceee5a68
Jonghakseo <unqocn@gmail.com>
2022-07-16 17:40:03 +0900

    Revert "add chakra"
    This reverts commit 4711a854f3506d3a38b5d447d14aecd22186dabb.

--- src/pages/content/view/app.tsx
+++ src/pages/content/view/app.tsx
@@ -1,14 +1,9 @@
 import { useEffect } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App() {
   useEffect(() => {
     console.log("content view loaded");
   }, []);
 
-  return (
-    <ChakraProvider>
-      <div className="content-view">content view</div>
-    </ChakraProvider>
-  );
+  return <div className="content-view">content view</div>;
 }
