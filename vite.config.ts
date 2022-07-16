b82e73470e473ad712a82ca00ad49058ddc3ef1d
Jonghakseo <unqocn@gmail.com>
2022-07-16 17:39:49 +0900

    Revert "remove scss"
    This reverts commit 9507401b26424639ceb0e5775bf8ba36d4409088.

--- vite.config.ts
+++ vite.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
       input: {
         content: resolve(pagesDir, "content", "index.ts"),
         background: resolve(pagesDir, "background", "index.ts"),
+        contentStyle: resolve(pagesDir, "content", "style.scss"),
         popup: resolve(pagesDir, "popup", "index.html"),
         options: resolve(pagesDir, "options", "index.html"),
       },
